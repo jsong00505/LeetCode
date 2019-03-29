5ca1587061b4e86b3a9a0d0745944d3385bf11ec
jsong <jsong00505@gmail.com>
2019-03-28 19:53:40 -0700

    feat: Add another solution of letter combinations phone number

--- Algorithms/src/main/java/medium/l/LetterCombinationsPhoneNumber.java
+++ Algorithms/src/main/java/medium/l/LetterCombinationsPhoneNumber.java
@@ -10,34 +10,63 @@ import java.util.List;
  * @github https://github.com/jsong00505/
  * @linkedin https://www.linkedin.com/in/junesongskorea/
  * @email jsong00505@gmail.com
- * @challenge Medium - Letter Combinations of a Phone Number
- *     (https://leetcode.com/problems/letter-combinations-of-a-phone-number/)
+ * @challenge Medium - Letter Combinations of a Phone Number (https://leetcode.com/problems/letter-combinations-of-a-phone-number/)
  */
 public class LetterCombinationsPhoneNumber {
+
+  String[] letters = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
   public List<String> letterCombinations(String digits) {
-    String[] letters = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    List<String> result = new ArrayList<>();
+    backTracking(result, digits, new StringBuilder(), 0);
+    return result;
+  }
+
+  private void backTracking(
+          List<String> result,
+          String digits,
+          StringBuilder subLetters,
+          int index) {
+    if (index == digits.length()) {
+      result.add(subLetters.toString());
+      return;
+    }
+
+    int digit = Character.getNumericValue(digits.charAt(index));
+    String letterSet = letters[digit];
+    index++;
+
+    for (char c : letterSet.toCharArray()) {
+      StringBuilder newSubLetter = new StringBuilder();
+      newSubLetter.append(subLetters).append(c);
+      backTracking(result, digits, newSubLetter, index);
+    }
+  }
+
+  // This is my own solution without back tracking
+  public List<String> myOwnSolution(String digits) {
 
     int total = 1;
     for (char c : digits.toCharArray()) {
       int num = Character.getNumericValue(c);
-      total *= letters[num - 2].length();
+      total *= letters[num].length();
     }
 
     List<StringBuilder> tempResult = new ArrayList<>();
     int changer = 1;
     for (char c : digits.toCharArray()) {
       int num = Character.getNumericValue(c);
-      int divider = total / letters[num - 2].length() / changer;
+      int divider = total / letters[num].length() / changer;
       int letterIndex = 0;
       for (int i = 0; i < total; i++) {
         if (i != 0 && i % divider == 0) {
           letterIndex++;
         }
 
-        if (letterIndex == letters[num - 2].length()) {
+        if (letterIndex == letters[num].length()) {
           letterIndex = 0;
         }
-        char d = letters[num - 2].charAt(letterIndex);
+        char d = letters[num].charAt(letterIndex);
 
         if (tempResult.size() != total) {
           StringBuilder sb = new StringBuilder();
@@ -47,7 +76,7 @@ public class LetterCombinationsPhoneNumber {
           tempResult.get(i).append(d);
         }
       }
-      changer *= letters[num - 2].length();
+      changer *= letters[num].length();
     }
 
     List<String> result = new ArrayList<>();
