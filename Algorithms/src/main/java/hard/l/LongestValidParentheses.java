f91632b09c8686e0db927884e4fe3d1ef43ce019
jsong <jsong00505@gmail.com>
2019-04-01 17:45:17 -0700

    feat: Add right solutions(dp, stack and etc.) of longest valid parentheses with more unit tests

--- Algorithms/src/main/java/hard/l/LongestValidParentheses.java
+++ Algorithms/src/main/java/hard/l/LongestValidParentheses.java
@@ -13,13 +13,111 @@ import java.util.Stack;
  *     (https://leetcode.com/problems/longest-valid-parentheses/)
  */
 public class LongestValidParentheses {
+
+  // dp
   public int longestValidParentheses(String s) {
+    int[] dp = new int[s.length()];
+    int longest = 0;
+    for (int i = 1; i < s.length(); i++) {
+      if (s.charAt(i) == ')') {
+        int prev = i - dp[i - 1] - 1;
+        if (prev >= 0 && s.charAt(prev) == '(') {
+          dp[i] = dp[i - 1] + 2;
+          if (prev - 1 >= 0) {
+            dp[i] += dp[prev - 1];
+          }
+        }
+      }
+      longest = Math.max(dp[i], longest);
+    }
+    return longest;
+  }
+
+  // stack
+  public int longestValidParenthesesByStack(String s) {
+
+    int longest = 0;
+    Stack<Integer> stack = new Stack<>();
+    stack.push(-1);
+
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) == '(') {
+        stack.push(i);
+      } else {
+        stack.pop();
+        if (stack.isEmpty()) {
+          stack.push(i);
+        } else {
+          longest = Math.max(i - stack.peek(), longest);
+        }
+      }
+    }
+    return longest;
+  }
+
+  // left-right solution
+  public int leftAndRight(String s) {
+
+    // result
+    int longest = 0;
+
+    // number of open/close bracket
+    int open = 0;
+    int close = 0;
+
+    // search left to right
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) == ')') {
+        // if the number of open bracket is smaller than close one the parentheses are not valid
+        // "())"
+        // "()())"
+        if (open <= close) {
+          open = 0;
+          close = 0;
+        } else {
+          close = close + 1;
+        }
+      } else {
+        open++;
+      }
+      if (open == close) {
+        longest = Math.max(longest, open);
+      }
+    }
+
+    // search right to left
+    open = 0;
+    close = 0;
+    for (int i = s.length() - 1; i >= 0; i--) {
+      // if the number of close bracket is smaller than open one the parentheses are not valid
+      // "(()"
+      // "(()()"
+      if (s.charAt(i) == '(') {
+        if (close <= open) {
+          open = 0;
+          close = 0;
+        } else {
+          open = open + 1;
+        }
+      } else {
+        close++;
+      }
+      if (open == close) {
+        longest = Math.max(longest, open);
+      }
+    }
+
+    return longest * 2;
+  }
+
+  // my own solution but too slow
+  public int myOwnSolution(String s) {
     int begin = 0;
     int end = s.length() - 1;
-    while (s.charAt(begin) != '(' && begin < end) {
+    while (begin < end && s.charAt(begin) != '(') {
       begin++;
     }
-    while (s.charAt(end) != ')' && begin < end) {
+    while (begin < end && s.charAt(end) != ')') {
       end--;
     }
 
@@ -31,11 +129,14 @@ public class LongestValidParentheses {
       }
       if (length == 0) {
         begin++;
-        while (s.charAt(begin) != '(' && begin < end) {
-          begin++;
-        }
+
+      } else {
+        begin += length;
       }
-      begin += length;
+
+      while (begin < end && s.charAt(begin) != '(') {
+        begin++;
+      }
     }
     return longest;
   }
@@ -47,14 +148,17 @@ public class LongestValidParentheses {
     int validCount = 0;
     while (index <= end) {
 
-      if (s.charAt(begin) == ')') {
+      if (s.charAt(index) == ')') {
         stack.pop();
       } else {
-        stack.push(')');
+        stack.push('(');
       }
       index++;
       if (stack.isEmpty()) {
         validCount = index - begin;
+        if (index <= end && s.charAt(index) == ')') {
+          break;
+        }
       }
     }
 
--- Algorithms/src/test/java/hard/l/LongestValidParenthesesTest.java
+++ Algorithms/src/test/java/hard/l/LongestValidParenthesesTest.java
@@ -15,9 +15,114 @@ public class LongestValidParenthesesTest {
   }
 
   @Test
-  public void longestValidParentheses() {
+  public void test1LongestValidParentheses() {
     // test input
     String s = "(()";
+
+    // set expected value
+    int expected = 2;
+
+    // perform test
+    int actual = longestValidParentheses.longestValidParentheses(s);
+
+    // assertion
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void test2LongestValidParentheses() {
+    // test input
+    String s = ")()())";
+
+    // set expected value
+    int expected = 4;
+
+    // perform test
+    int actual = longestValidParentheses.longestValidParentheses(s);
+
+    // assertion
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void test3LongestValidParentheses() {
+    // test input
+    String s = "";
+
+    // set expected value
+    int expected = 0;
+
+    // perform test
+    int actual = longestValidParentheses.longestValidParentheses(s);
+
+    // assertion
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void test4LongestValidParentheses() {
+    // test input
+    String s = "(()()))(())()())()((()))))";
+
+    // set expected value
+    int expected = 8;
+
+    // perform test
+    int actual = longestValidParentheses.longestValidParentheses(s);
+
+    // assertion
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void test5LongestValidParentheses() {
+    // test input
+    String s = ")))))))(";
+
+    // set expected value
+    int expected = 0;
+
+    // perform test
+    int actual = longestValidParentheses.longestValidParentheses(s);
+
+    // assertion
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void test6LongestValidParentheses() {
+    // test input
+    String s = ")";
+
+    // set expected value
+    int expected = 0;
+
+    // perform test
+    int actual = longestValidParentheses.longestValidParentheses(s);
+
+    // assertion
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void test7LongestValidParentheses() {
+    // test input
+    String s = "(";
+
+    // set expected value
+    int expected = 0;
+
+    // perform test
+    int actual = longestValidParentheses.longestValidParentheses(s);
+
+    // assertion
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void test8LongestValidParentheses() {
+    // test input
+    String s = "()(()";
 
     // set expected value
     int expected = 2;
